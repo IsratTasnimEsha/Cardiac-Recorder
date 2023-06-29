3e435bd3e58a2534d970d22131719d7c4d8a7920
Esha <israttasnimesha1046@gmail.com>
2023-06-29 16:00:00 +0600

    add

--- app/src/main/java/com/example/gitproject/UserClass.java
+++ app/src/main/java/com/example/gitproject/UserClass.java
@@ -3,6 +3,19 @@ package com.example.gitproject;
 public class UserClass {
     String Phone, Systolic, Diastolic, Heart_Rate, Comment, Time;
 
+    public UserClass() {
+
+    }
+
+    public UserClass(String phone, String systolic, String diastolic, String heart_Rate, String comment, String time) {
+        Phone = phone;
+        Systolic = systolic;
+        Diastolic = diastolic;
+        Heart_Rate = heart_Rate;
+        Comment = comment;
+        Time = time;
+    }
+
     public String getPhone() {
         return Phone;
     }
