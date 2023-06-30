50926dba6e2c3a5de34b650b691a6775a9e6360a
Esha <israttasnimesha1046@gmail.com>
2023-07-01 00:25:56 +0600

    doc

--- app/src/main/java/com/example/gitproject/RecordList.java
+++ app/src/main/java/com/example/gitproject/RecordList.java
@@ -4,14 +4,30 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Represents a list of health records.
+ */
+
 public class RecordList {
     ArrayList<UserClass>arrayList = new ArrayList<>();
 
+    /**
+     * Returns the list of health records.
+     *
+     * @return
+     *      The list of health records.
+     */
     public ArrayList<UserClass> getRecords() {
         ArrayList<UserClass> recordList = arrayList;
         return recordList;
     }
 
+    /**
+     * Adds one's health records.
+     *
+     * @param userClass
+     *      The health record to be added.
+     */
     public void add(UserClass userClass) {
         if(arrayList.contains(userClass))
         {
@@ -20,6 +36,12 @@ public class RecordList {
         arrayList.add(userClass);
     }
 
+    /**
+     * Deletes one's health records.
+     *
+     * @param userClass
+     *      The health record to be deleted.
+     */
     public void delete(UserClass userClass)
     {
         if(!arrayList.contains(userClass))
@@ -29,6 +51,14 @@ public class RecordList {
         arrayList.remove(userClass);
     }
 
+    /**
+     * Updates the health records with new data at the specified position.
+     *
+     * @param position
+     *      The position at which the records are being updated.
+     * @param userClass
+     *      The updated health record.
+     */
     public void update(int position, UserClass userClass)
     {
         arrayList.set(position, userClass);
